0ca946fdf656e8d89c0e0321fe6f90e06f383c9a
Russell Matney <russell.matney@gmail.com>
2024-03-09 15:47:09 -0500

    test: impl some basic tests

--- test/log_test.gd
+++ test/log_test.gd
@@ -1,6 +1,39 @@
 extends GdUnitTestSuite
 
-func test_log():
-	var val = Log.to_printable([1, 2, 3])
+var regex
+var regex2
 
-	assert_that(val).is_equal("[1, 2, 3]")
+func before():
+	regex = RegEx.new()
+	regex.compile("\\[([^\\\\]\\\\]*)\\]")
+
+## helpers ##########################################
+
+func bb_sub(val):
+	# val = regex.sub(val, "<1>", true)
+	return val
+
+func to_test_input(val):
+	return bb_sub(Log.to_printable(val))
+
+func debug_output(val):
+	print(val)
+	# for x in val:
+	# 	printraw(x)
+
+## tests ##########################################
+
+func test_log_params():
+	var val = to_test_input([1, 2, 3])
+	debug_output(val)
+	assert_that(val).is_equal("1 2 3")
+
+func test_log_array():
+	var val = to_test_input([[1, 2, 3]])
+	debug_output(val)
+	assert_that(val).is_equal("[color=red][ [/color]1[color=red], [/color]2[color=red], [/color]3[color=red] ][/color]")
+
+func test_log_vector():
+	var val = to_test_input([Vector2(1, 2)])
+	debug_output(val)
+	assert_that(val).is_equal("([color=purple]1[/color],[color=purple]2[/color])")
