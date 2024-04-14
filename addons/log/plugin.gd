ce88e7d965959c1535f00a1b9211e8069a18ff62
Russell Matney <russell.matney@gmail.com>
2024-04-13 23:45:08 -0400

    chore: drop editor plugin enter/exit
    No need for this at the moment.

--- addons/log/plugin.gd
+++ addons/log/plugin.gd
@@ -2,18 +2,18 @@
 extends EditorPlugin
 
 
-func _enter_tree():
-	Log.pr("<Log> config:", Log.config)
+# func _enter_tree():
+# 	Log.pr("<Log> config:", Log.config)
 
-	# parsing command line args
-	# var arguments = {}
-	# for argument in OS.get_cmdline_args():
-	# 	if argument.find("=") > -1:
-	# 		var key_value = argument.split("=")
-	# 		arguments[key_value[0].lstrip("--")] = key_value[1]
-	# 	else:
-	# 		arguments[argument.lstrip("--")] = true
-	# Log.pr("args", arguments)
+# 	# parsing command line args
+# 	# var arguments = {}
+# 	# for argument in OS.get_cmdline_args():
+# 	# 	if argument.find("=") > -1:
+# 	# 		var key_value = argument.split("=")
+# 	# 		arguments[key_value[0].lstrip("--")] = key_value[1]
+# 	# 	else:
+# 	# 		arguments[argument.lstrip("--")] = true
+# 	# Log.pr("args", arguments)
 
-func _exit_tree():
-	Log.pr("</Log>")
+# func _exit_tree():
+# 	Log.pr("</Log>")
