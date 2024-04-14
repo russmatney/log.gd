2bd6de4e5fa349b6d2663c1887b7bdbdc448b08c
Russell Matney <russell.matney@gmail.com>
2024-04-13 23:46:09 -0400

    deps: drop gd-plug-ui
    I'm not using this anyway - dropping while i resolve whatever's up with CI.

--- plug.gd
+++ plug.gd
@@ -1,8 +1,4 @@
 extends "res://addons/gd-plug/plug.gd"
 
 func _plugging():
-	# Declare plugins with plug(repo, args)
-	# plug("imjp94/gd-YAFSM") # By default, gd-plug will only install anything from "addons/" directory
-	# plug("imjp94/gd-YAFSM", {"include": ["addons/"]}) # By default, gd-plug will only install anything from "addons/" directory
 	plug("MikeSchulze/gdUnit4")
-	plug("imjp94/gd-plug-ui")
