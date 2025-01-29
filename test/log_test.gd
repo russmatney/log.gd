e92dc22bbe55eed8e0177746302abb82ce13f992
Russell Matney <russell.matney@gmail.com>
2025-01-29 11:50:25 -0500

    fix: resolve type warnings in test file
    Also disables a few more warnings - no need to handle return values in
    test cases, and exclude_addons for now so we don't see a million gdunit
    type warnings.
    
    It should be noted that exclude_addons should be toggled while working
    on log.gd to see the errors in that file.

--- test/log_test.gd
+++ test/log_test.gd
@@ -2,14 +2,14 @@ extends GdUnitTestSuite
 
 ## null ##########################################
 
-func test_null():
-	var val = Log.to_pretty(null)
+func test_null() -> void:
+	var val: String = Log.to_pretty(null)
 	assert_str(val).is_equal("[color=pink]<null>[/color]")
 
 ## bool ##########################################
 
-func test_bool():
-	var val = Log.to_pretty(true)
+func test_bool() -> void:
+	var val: String = Log.to_pretty(true)
 	assert_str(val).is_equal("[color=pink]true[/color]")
 
 	val = Log.to_pretty(false)
@@ -17,94 +17,94 @@ func test_bool():
 
 ## strings ##########################################
 
-func test_log_strings():
-	var val = Log.to_pretty("Special")
+func test_log_strings() -> void:
+	var val: String = Log.to_pretty("Special")
 	assert_str(val).is_equal("[color=pink]Special[/color]")
 
-func test_log_string_names():
-	var val = Log.to_pretty(&"Special")
+func test_log_string_names() -> void:
+	var val: String = Log.to_pretty(&"Special")
 	assert_str(val).is_equal("[color=orange]&[/color]\"Special\"")
 
-func test_node_paths():
-	var np = NodePath("Some/Path/To/A/Node")
-	var val = Log.to_pretty(np)
+func test_node_paths() -> void:
+	var np: NodePath = NodePath("Some/Path/To/A/Node")
+	var val: String = Log.to_pretty(np)
 	assert_str(val).is_equal("[color=orange]^[/color]\"Some/Path/To/A/Node\"")
 
-func test_already_colorized_string():
-	var val = Log.to_pretty("[color=blue]Something[/color]")
+func test_already_colorized_string() -> void:
+	var val: String = Log.to_pretty("[color=blue]Something[/color]")
 	assert_str(val).is_equal("[color=blue]Something[/color]")
 
 ## color ##########################################
 
-func test_color():
+func test_color() -> void:
 	# test for printing colors, not log.gd color nuances
 	pass
 
 ## numbers ##########################################
 
-func test_log_ints():
-	var val = Log.to_pretty(1)
+func test_log_ints() -> void:
+	var val: String = Log.to_pretty(1)
 	assert_str(val).is_equal("[color=green]1[/color]")
 
-func test_log_floats():
-	var val = Log.to_pretty(3.14)
+func test_log_floats() -> void:
+	var val: String = Log.to_pretty(3.14)
 	assert_str(val).is_equal("[color=green]3.14[/color]")
 
 # TYPE_MAX
 
 ## vectors ###############################################
 
-func test_log_vector2():
-	var val = Log.to_pretty(Vector2(1, 2))
+func test_log_vector2() -> void:
+	var val: String = Log.to_pretty(Vector2(1, 2))
 	assert_str(val).is_equal("[color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red])[/color]")
 
-func test_log_vector2i():
-	var val = Log.to_pretty(Vector2i(1, 2))
+func test_log_vector2i() -> void:
+	var val: String = Log.to_pretty(Vector2i(1, 2))
 	assert_str(val).is_equal(
 		"[color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red])[/color]"
 		)
 
-func test_log_vector3():
-	var val = Log.to_pretty(Vector3(1, 2, 3))
+func test_log_vector3() -> void:
+	var val: String = Log.to_pretty(Vector3(1, 2, 3))
 	assert_str(val).is_equal(
 		"[color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red],[/color][color=green]3[/color][color=red])[/color]"
 		)
 
-func test_log_vector3i():
-	var val = Log.to_pretty(Vector3i(1, 2, 3))
+func test_log_vector3i() -> void:
+	var val: String = Log.to_pretty(Vector3i(1, 2, 3))
 	assert_str(val).is_equal(
 		"[color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red],[/color][color=green]3[/color][color=red])[/color]"
 		)
 
-func test_log_vector4():
-	var val = Log.to_pretty(Vector4(1, 2, 3, 4))
+func test_log_vector4() -> void:
+	var val: String = Log.to_pretty(Vector4(1, 2, 3, 4))
 	assert_str(val).is_equal(
 		"[color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red],[/color][color=green]3[/color][color=red],[/color][color=green]4[/color][color=red])[/color]"
 		)
 
-func test_log_vector4i():
-	var val = Log.to_pretty(Vector4i(1, 2, 3, 4))
+func test_log_vector4i() -> void:
+	var val: String = Log.to_pretty(Vector4i(1, 2, 3, 4))
 	assert_str(val).is_equal(
 		"[color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red],[/color][color=green]3[/color][color=red],[/color][color=green]4[/color][color=red])[/color]"
 		)
 
 ## rect ###############################################
 
-func test_rect():
+func test_rect() -> void:
 	# TODO
 	pass
 
-func test_rect2i():
+func test_rect2i() -> void:
 	# TODO
 	pass
 
 ## transform ###############################################
 
-func test_transform2d():
+func test_transform2d() -> void:
 	# TODO
 	pass
 
-func test_transform3d():
+func test_transform3d() -> void:
 	# TODO
 	pass
 
@@ -116,15 +116,15 @@ func test_transform3d():
 
 ## arrays ##########################################
 
-func test_log_array():
-	var val = Log.to_pretty([1, 2, 3])
+func test_log_array() -> void:
+	var val: String = Log.to_pretty([1, 2, 3])
 	assert_str(val).is_equal(
 		"[color=red][ [/color][color=green]1[/color][color=red], [/color][color=green]2[/color][color=red], [/color][color=green]3[/color][color=red] ][/color]")
 
 # TYPE_PACKED_STRING_ARRAY
-func test_log_packed_string_array():
-	var packedStrArray = PackedStringArray(["hi", "there"])
-	var val = Log.to_pretty(packedStrArray)
+func test_log_packed_string_array() -> void:
+	var packedStrArray: PackedStringArray = PackedStringArray(["hi", "there"])
+	var val: String = Log.to_pretty(packedStrArray)
 	assert_str(val).is_equal(
 		"[color=red][ [/color][color=pink]hi[/color][color=red], [/color][color=pink]there[/color][color=red] ][/color]")
 
@@ -139,19 +139,19 @@ func test_log_packed_string_array():
 
 ## dictionaries ##########################################
 
-func test_log_dictionary():
-	var val = Log.to_pretty({some="val", another=2})
+func test_log_dictionary() -> void:
+	var val: String = Log.to_pretty({some="val", another=2})
 	assert_str(val).is_equal(
 		"[color=red]{ [/color][color=magenta]\"some\"[/color]: [color=pink]val[/color][color=red], [/color][color=magenta]\"another\"[/color]: [color=green]2[/color][color=red] }[/color]")
 
-func test_array_of_dictionaries():
-	var val = Log.to_pretty([{some="val"},{some="another"}])
+func test_array_of_dictionaries() -> void:
+	var val: String = Log.to_pretty([{some="val"},{some="another"}])
 	assert_str(val).is_equal(
 		"[color=red][ [/color][color=red]{ [/color][color=magenta]\"some\"[/color]: [color=pink]val[/color][color=red] }[/color][color=red], [/color][color=red]{ [/color][color=magenta]\"some\"[/color]: [color=pink]another[/color][color=red] }[/color][color=red] ][/color]"
 		)
 
-func test_array_of_dictionaries_with_newlines():
-	var val = Log.to_pretty([{some="val"},{some="another"}], {newlines=true})
+func test_array_of_dictionaries_with_newlines() -> void:
+	var val: String = Log.to_pretty([{some="val"},{some="another"}], {newlines=true})
 	assert_str(val).is_equal(
 		"[color=red][ [/color][color=red]{ [/color][color=magenta]\"some\"[/color]: [color=pink]val[/color][color=red] }[/color][color=red], [/color][color=red]{ [/color][color=magenta]\"some\"[/color]: [color=pink]another[/color][color=red] }[/color][color=red] ][/color]"
 		)
@@ -159,17 +159,17 @@ func test_array_of_dictionaries_with_newlines():
 ## custom object ##########################################
 
 class ExampleObj:
-	var val
-	func _init(v):
+	var val: Variant
+	func _init(v: Variant) -> void:
 		val = v
 
-	func to_pretty():
+	func to_pretty() -> Variant:
 		return {val=val, id=get_instance_id()}
 
-func test_custom_to_pretty():
-	var obj = ExampleObj.new(Vector2(1, 2))
-	var val = Log.to_pretty(obj)
-	var id = obj.get_instance_id()
+func test_custom_to_pretty() -> void:
+	var obj: ExampleObj = ExampleObj.new(Vector2(1, 2))
+	var val: String = Log.to_pretty(obj)
+	var id: int = obj.get_instance_id()
 	assert_str(val).is_equal(
 		"[color=red]{ [/color][color=magenta]\"val\"[/color]: [color=red]([/color][color=green]1[/color][color=red],[/color][color=green]2[/color][color=red])[/color][color=red], [/color][color=magenta]\"id\"[/color]: [color=green]%s[/color][color=red] }[/color]"
 		% str(id))
@@ -184,36 +184,36 @@ func test_custom_to_pretty():
 
 ## custom resource ##########################################
 
-func test_custom_resource():
-	var tp = TestPlayer.new()
+func test_custom_resource() -> void:
+	var tp: TestPlayer = TestPlayer.new()
 	tp.name = "Hanz"
 	tp.level = 3
 	tp.role = TestPlayer.Role.Tank
 
-	var val = Log.to_pretty(tp)
+	var val: String = Log.to_pretty(tp)
 	assert_str(val).is_equal(
 		"[color=magenta]TestPlayer.gd[/color]"
 		)
 
-func test_custom_resource_register_overwrite():
-	var tp = TestPlayer.new()
+func test_custom_resource_register_overwrite() -> void:
+	var tp: TestPlayer = TestPlayer.new()
 	tp.name = "Hanz"
 	tp.level = 3
 	tp.role = TestPlayer.Role.Tank
 
-	Log.register_type_overwrite(tp.get_class(), func(msg, _opts):
+	Log.register_type_overwrite(tp.get_class(), func(msg: Variant, _opts: Dictionary) -> String:
 		return Log.to_pretty({name=msg.name, level=msg.level}))
 
-	var val = Log.to_pretty(tp)
+	var val: String = Log.to_pretty(tp)
 	assert_str(val).is_equal(
 		"[color=red]{ [/color][color=magenta]\"name\"[/color]: [color=pink]Hanz[/color][color=red], [/color][color=magenta]\"level\"[/color]: [color=green]3[/color][color=red] }[/color]"
 		)
 
 ## color schemes ##########################################
 
-func test_null_alt_colors():
+func test_null_alt_colors() -> void:
 	Log.set_colors_termsafe()
-	var val = Log.to_pretty(null)
+	var val: String = Log.to_pretty(null)
 	assert_str(val).is_equal("[color=pink]<null>[/color]")
 
 	Log.set_colors_pretty()
@@ -224,16 +224,16 @@ func test_null_alt_colors():
 	val = Log.to_pretty(null)
 	assert_str(val).is_equal("[color=pink]<null>[/color]")
 
-func test_color_overwriting():
-	var val = Log.to_pretty(null, {color_theme={TYPE_NIL: "red"}})
+func test_color_overwriting() -> void:
+	var val: String = Log.to_pretty(null, {color_theme={TYPE_NIL: "red"}})
 	assert_str(val).is_equal("[color=red]<null>[/color]")
 
 	# does not clear other colors
 	val = Log.to_pretty(1, {color_theme={TYPE_NIL: "red"}})
 	assert_str(val).is_equal("[color=green]1[/color]")
 
-func test_color_scheme_overwriting():
-	var val = Log.to_pretty(null)
+func test_color_scheme_overwriting() -> void:
+	var val: String = Log.to_pretty(null)
 	assert_str(val).is_equal("[color=pink]<null>[/color]")
 
 	Log.merge_theme_overwrites({TYPE_NIL: "red"})
@@ -249,11 +249,11 @@ func test_color_scheme_overwriting():
 	Log.set_colors_termsafe()
 	Log.clear_theme_overwrites()
 
-func test_disable_colors_to_pretty():
+func test_disable_colors_to_pretty() -> void:
 	assert_str(Log.to_pretty(1)).is_equal("[color=green]1[/color]")
 	assert_str(Log.to_pretty(1, {disable_colors=true})).is_equal("1")
 
-func test_disable_colors_via_config():
+func test_disable_colors_via_config() -> void:
 	assert_str(Log.to_pretty(1)).is_equal("[color=green]1[/color]")
 
 	Log.disable_colors()
