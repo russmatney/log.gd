8171e27aac358e46bce5afef6838ca3ac5c42cbe
Russell Matney <russell.matney@gmail.com>
2025-04-28 10:59:45 -0400

    wip: better showcase

--- src/ExampleScene.gd
+++ src/ExampleScene.gd
@@ -4,9 +4,10 @@ extends CanvasLayer
 func _enter_tree() -> void:
 	# print("\\033[31mHello\\033[0m")
 
-	# Log.set_colors_pretty()
+	Log.set_colors_pretty()
 	# Log.disable_colors()
 
+	# print(Log.config)
 	Log.info(Log.config)
 
 class ExampleObj:
@@ -44,3 +45,39 @@ func _ready() -> void:
 	print_rich(Log.to_pretty(1, {disable_colors=true}))
 
 	# print("\\033[31;1;4mHello\\033[0m")
+
+	run_showcase()
+
+func print_header(header: String) -> void:
+	print(str("\n\n\t====", header, "====\n\n"))
+
+func print_and_log(v: Variant) -> void:
+	print(v)
+	Log.pr(v)
+
+func run_showcase() -> void:
+	print_header("SHOWCASE")
+
+	print_header("Ints, Floats")
+	print_and_log(1)
+	print_and_log(1.0)
+	Log.pr(0.0, 1.0, 3.14)
+
+	print_header("Vectors")
+	print_and_log(Vector2())
+	print_and_log(Vector3(2.3, 1.4, 0.5))
+	print_and_log(Vector2i.LEFT)
+	print_and_log(Vector3i.UP)
+
+	print_header("Strings, String Names")
+	print_and_log("Hello, World!")
+	print_and_log(&"Hi there!")
+	Log.pr("Hello", &"World")
+
+	print_header("Arrays")
+	print_and_log([1, 2.0, 3, 4.0])
+	Log.pr(1, 2.0, [3, 4.0])
+	Log.prn([1, 2.0, 3, 4.0])
+
+	print_header("Dictionaries")
+	print_and_log({name="Arthur", quest="I seek the grail", health=0.7})
